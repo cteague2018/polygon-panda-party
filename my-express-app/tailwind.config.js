3faf78ab16fff8c975616eff58c1845c22ea97b7
cteague2018 <45378989+cteague2018@users.noreply.github.com>
2024-12-13 15:40:27 -0700

    Deploying to Git and Vercel on 2024-12-13 15:40:27

--- my-express-app/tailwind.config.js
+++ my-express-app/tailwind.config.js
@@ -1,15 +1,15 @@
-module.exports = {    
-  content: [    
-    "./src/**/*.{js,jsx,ts,tsx}",    
-  ],    
-  theme: {    
-    extend: {    
-      fontFamily: {    
-        'bubblegum': ['"Bubblegum Sans"', 'cursive'],    
-      },    
-    },    
-  },    
-  plugins: [    
-    require('@tailwindcss/typography'),    
-  ],    
+/** @type {import('tailwindcss').Config} */  
+module.exports = {  
+  content: [  
+    "./src/**/*.{js,jsx,ts,tsx}",  
+    "./public/index.html"  
+  ],  
+  theme: {  
+    extend: {  
+      fontFamily: {  
+        bubblegum: ['Bubblegum Sans', 'cursive'],  
+      },  
+    },  
+  },  
+  plugins: [],  
 }  
